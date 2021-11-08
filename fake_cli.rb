bd5b0b3f0b0f0932c3dc83c02e952ba933c1ddb6
SelfTaughtCoder78704 <bobbynicholson78704@gmail.com>
2021-11-07 18:04:37 -0600

    added custom template functionality, TODO, add support for Sections

--- fake_cli.rb
+++ fake_cli.rb
@@ -1,4 +1,5 @@
 # program to create new files in ruby and puts them in correct directory
+
 def new_page_setup(page_name)
   File.open("#{Dir.pwd}/app/assets/javascripts/pages/#{page_name}.js", "w+") {|f| f.write("//= require jquery3
   //= require libs/tracking
@@ -14,7 +15,45 @@ def new_page_setup(page_name)
   puts "The Yaml file was created empty, the Javascript and Stylesheet have enough to make the server work."
   return
 end
-puts "New page creation:"
-puts "Enter the name of the page. Ex. overview_a"
-page_name = gets.chomp
-new_page_setup(page_name)
+
+def new_template_setup(template_name)
+  File.open("#{Dir.pwd}/app/views/custom/#{template_name}.html.erb", "w")
+  puts "#{template_name}.html.erb created."
+  return
+end
+    
+def start
+  puts "What would you like to create?"
+  puts "1. New Page"
+  puts "2. New Template"
+  puts "3. Exit"
+  answer = gets.chomp
+  if answer == "1"
+    puts "What is the name of the page?"
+    page_name = gets.chomp
+    new_page_setup(page_name)
+  elsif answer == "2"
+    puts "What is the name of the template?"
+    template_name = gets.chomp
+    new_template_setup(template_name)
+  elsif answer == "3"
+    puts "Goodbye!"
+    exit
+  else
+    puts "Invalid input."
+    exit
+  end
+  puts "Done!"
+  puts "Would you like to create anything else?"
+  puts "Type yes or no"
+  new_answer = gets.chomp
+  if new_answer == "yes"
+    start()
+  else
+    puts "Goodbye!"
+    exit
+  end
+end
+
+
+start()
